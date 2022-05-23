92d59c382603158b9686384437390b33bdef5b2b
Student <scncgz@gmail.com>
2022-05-23 22:37:28 +0000

    quicmuxconn impl

--- go.mod
+++ go.mod
@@ -11,7 +11,22 @@ require (
 )
 
 require (
+	github.com/cheekybits/genny v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.1 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.1 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lucas-clemente/quic-go v0.27.1
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/transport v0.13.0 // indirect
 	github.com/pion/udp v0.1.1 // indirect
--- net.go
+++ net.go
@@ -1,8 +1,10 @@
 package socks6
 
 import (
+	"context"
 	"net"
 
+	"github.com/lucas-clemente/quic-go"
 	"github.com/studentmain/socks6/internal"
 )
 
@@ -97,9 +99,80 @@ func (u dtlsDatagram) RemoteAddr() net.Addr {
 }
 
 type MultiplexedConn interface {
-	Accept() net.Conn
-	Dial() net.Conn
+	Accept() (net.Conn, error)
+	Dial() (net.Conn, error)
 	LocalAddr() net.Addr
 	RemoteAddr() net.Addr
 	Close() error
+}
+
+type quicMuxConn struct {
+	conn quic.Connection
+}
+
+var _ MultiplexedConn = quicMuxConn{}
+var _ SeqPacket = quicMuxConn{}
+
+func (u quicMuxConn) Accept() (net.Conn, error) {
+	qs, err := u.conn.AcceptStream(context.Background())
+	return quicConn{Connection: u.conn, Stream: qs}, err
+}
+
+func (u quicMuxConn) Dial() (net.Conn, error) {
+	qs, err := u.conn.OpenStream()
+	return quicConn{Connection: u.conn, Stream: qs}, err
+}
+
+func (u quicMuxConn) Close() error {
+	// noop
+	return nil
+}
+
+func (u quicMuxConn) NextDatagram() (Datagram, error) {
+	data, err := u.conn.ReceiveMessage()
+	if err != nil {
+		return nil, err
+	}
+
+	dgram := quicDatagram{
+		data: data,
+		conn: u.conn,
+	}
+	return dgram, nil
+}
+func (u quicMuxConn) Reply(b []byte) error {
+	return u.conn.SendMessage(b)
+}
+func (u quicMuxConn) LocalAddr() net.Addr {
+	return u.conn.LocalAddr()
+}
+func (u quicMuxConn) RemoteAddr() net.Addr {
+	return u.conn.RemoteAddr()
+}
+
+type quicConn struct {
+	quic.Stream
+	quic.Connection
+}
+
+var _ net.Conn = quicConn{}
+
+type quicDatagram struct {
+	data []byte
+	conn quic.Connection
+}
+
+var _ Datagram = quicDatagram{}
+
+func (u quicDatagram) Data() []byte {
+	return u.data
+}
+func (u quicDatagram) Reply(b []byte) error {
+	return u.conn.SendMessage(b)
+}
+func (u quicDatagram) LocalAddr() net.Addr {
+	return u.conn.LocalAddr()
+}
+func (u quicDatagram) RemoteAddr() net.Addr {
+	return u.conn.RemoteAddr()
 }
